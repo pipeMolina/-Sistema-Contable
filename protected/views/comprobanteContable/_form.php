05951e1f5a7e97cff9de89ea71be69d0eff1dd0a
pipe <pipe_lqs18@hotmail.com>
2018-04-25 05:59:50 -0300

    mejorando vista comprobante

--- protected/views/comprobanteContable/_form.php
+++ protected/views/comprobanteContable/_form.php
@@ -103,25 +103,8 @@
 <div id="resultado"></div>
 
 
- <script type="text/javascript">  
+<script type="text/javascript">  
 
-	/*function total(n,obj)
-	{ 	
-	   	if(n==1)
-	   	{
-	   		document.getElementById('hiddenD').value = parseInt(obj)+parseInt(document.getElementById('hiddenD').value);
-	   		$("#totalDebe").html(document.getElementById('hiddenD').value);
-
-       	   	$('#validaDebe'+i+'').empty();
-       	   	$('#validaDebe'+i+'').removeClass("alert-danger");
-	   	}
-	  	else
-	   	{
-	   		document.getElementById('hiddenH').value = parseInt(obj)+parseInt(document.getElementById('hiddenH').value);
-	   		$("#totalHaber").html(document.getElementById('hiddenH').value);
-
-	   	}
-	} */ 
  $(document).ready(function(){ 
  	  /*Variables Globales*/
  	  var i=0;
@@ -138,40 +121,9 @@
 	           	$("#mensaje").html("Debe elegir Empresa");
 				$("#mensaje").addClass("alert alert-dismissible alert-warning");
            } 
-           else if(i>0)
+           else
            {
-           	   valueDebe=$("#debe"+i+'').val();
-           	   valueHaber=$("#haber"+i+'').val();
-           	   if($("#dropDown"+i+'').val() == "")
-           	   {
-           	   		$("#validaDropDown"+i+'').html("Debe elegir cuenta");
-		    		$("#validaDropDown"+i+'').addClass("alert-danger");
-           	   }
-		       else if (!/^([0-9])*$/.test(valueDebe))
-		    	{
-		    		$("#validaDebe"+i+'').html("Se permite solo numeros");
-		    		$("#validaDebe"+i+'').addClass("alert-danger");
-		    	}	 
-		    	 else if(!/^([0-9])*$/.test(valueHaber))
-		    	{
-		    		$("#validaHaber"+i+'').html("Se permite solo numeros");
-		    		$("#validaHaber"+i+'').addClass("alert-danger");
-		    	} 
-		    	else
-		    	{
-		    		$("#mensaje").empty();
-           	   		$("#mensaje").removeClass("alert alert-dismissible alert-warning");
-
-           	   		$("#validaDropDown"+i+'').empty();
-		    		$("#validaDropDown"+i+'').removeClass("alert-danger");
-           	   		
-           	   		$("#validaDebe"+i+'').empty();
-           	   		$("#validaDebe"+i+'').removeClass("alert-danger");
-           	   		
-           	   		$("#validaHaber"+i+'').empty();
-           	   		$("#validaHaber"+i+'').removeClass("alert-danger");
-           	   		i++;
-
+			       i++;
            	   	   var lineas=$('<div class="form-group" id="grupoLineas'+i+'"></div>');
 		      	   var divDropDownList=$('<div class=\"col-lg-4\" id="row'+i+'"></div>');
 		      	   var divDebe=$('<div class=\"col-lg-3\" id="row'+i+'"></div>');
@@ -206,50 +158,8 @@
 			               		$('#dropDown'+i+'').html(data);
 			               	}
 			            });
-		    	}
            }	
       
-    		else 
-    		{
-
-           	  /*Agregando filas para el primer caso*/
-           	   $("#mensaje").empty();
-           	   $("#mensaje").removeClass("alert alert-dismissible alert-warning");
-			   i++;
-	      	   var lineas=$('<div class="form-group" id="grupoLineas'+i+'"></div>');
-		      	   var divDropDownList=$('<div class=\"col-lg-4\" id="row'+i+'"></div>');
-		      	   var divDebe=$('<div class=\"col-lg-3\" id="row'+i+'"></div>');
-		      	   var divHaber=$('<div class=\"col-lg-3\" id="row'+i+'"></div>');
-		      	   var divEliminar=$('<div class=\"col-lg-2\" id="row'+i+'"></div>');
-		           //alert('row'+i);
-		           var dropDown = $("<select id='dropDown"+i+"' name=\"Cuenta[]\" class=\"form-control\"/><div id='validaDropDown"+i+"'></div>");
-		           var debe=$("<input type='text' id='debe"+i+"' name=Debe[] value=0 class='form-control'><div id='validaDebe"+i+"'></div>");
-		           var haber=$("<input type='text' id='haber"+i+"' name=Haber[] value=0 class='form-control'><div id='validaHaber"+i+"'></div>");
-		           //var debe=$('<?php echo $form->textField($modelLinea,"DEBE[]",array("class"=>"form-control","onchange"=>"total(1,this.value);","id"=>"debe1"))?><div id="validaDebe'+i+'"></div>');
-		           //var haber=$('<?php echo $form->textField($modelLinea,"HABER[]",array("class"=>"form-control","onchange"=>"total(0,this.value,'i');"));?>');
-		           var eliminar=$('<button type="button" name="remove" id="'+i+'" class="btn btn_remove btn-danger">x</button>');
-		           divDropDownList.append(dropDown);
-		           divDebe.append(debe);
-		           divHaber.append(haber);
-		           divEliminar.append(eliminar);
-		           lineas.append(divDropDownList);
-		           lineas.append(divDebe);
-		           lineas.append(divHaber);
-		           lineas.append(divEliminar);
-			   $("#dynamic_field").append(lineas);
-				var url = "<?php echo CController::createUrl('ComprobanteContable/CargaCuentasJs'); ?>";
-		        $.ajax(
-		            {
-		               	type:"POST",
-		                url: url,
-		               	data:"id="+$("#rutEmpresa").val(),
-		               	dataType:"html",
-		               	success: function(data)
-		               	{
-		               		$('#dropDown'+i+'').html(data);
-		               	}
-		            });
-           	} 
       }); 
      /*Accion para el boton borrar*/
       $(document).on("click", ".btn_remove", function(){  
@@ -261,24 +171,53 @@
       /*Accion para el boton guardar*/
       $("#submit").click(function(){
 
-			//var sumaDebe=$("#hiddenD").val();
-			//var sumaHaber=$("#hiddenH").val();
            	var tipoComprobante = $("#tipoComprobante").val();
            	var x = i;
-           	while(x > 0)
+           	var z=i;
+           	var activar=1;
+           	if(tipoComprobante=="")
            	{
-           		sumaDebe += parseInt($("#debe"+x+'').val());
-           		sumaHaber += parseInt($("#haber"+x+'').val());
-           		x--;
+           		$("#mensaje").html("Debe elegir Tipo ");
+				$("#mensaje").addClass("alert alert-danger");
            	}
-	 		resultado = sumaDebe - sumaHaber;
-            if(tipoComprobante == "")
+           	while(x>0)
            	{
-           		$(".panel panel-primary").animate({scrollTop:0},'500');
-           		$("#mensaje").html("Debe elegir Tipo");
-				$("#mensaje").addClass("alert alert-dismissible alert-warning");
+	           	var dropDown=$('#dropDown'+x+'').val();
+	           	var valueDebe=$('#debe'+x+'').val();
+	           	var valueHaber=$('#haber'+x+'').val();
+	           	if(dropDown=="")
+	           	{
+	           		$('#validaDropDown'+x+'').html("Debe elegir Cuenta");
+	           		activar=0;
+	           	}
+	           	else if(!/^([0-9])*$/.test(valueDebe))
+	           	{
+	           		$('#validaDebe'+x+'').html("Se permite solo números");
+	           		activar=0;
+	           	}else if(!/^([0-9])*$/.test(valueHaber))
+	           	{
+	           		$('#validaHaber'+x+'').html("Se permite solo números");
+	           		activar=0;
+	           	}else
+	           	{
+	           		$('#validaDropDown'+x+'').empty();
+	           		$('#validaDebe'+x+'').empty();
+	           		$('#validaHaber'+x+'').empty();
+	           	}
+	           	x--;
            	}
-			else if(resultado == 0)
+           	if(activar==1)
+           	{
+	           	while(z > 0)
+	           	{
+	           		sumaDebe += parseInt($("#debe"+z+'').val());
+	           		sumaHaber += parseInt($("#haber"+z+'').val());
+	           		z--;
+	           	}	
+	 			resultado = sumaDebe - sumaHaber;
+           	}	
+	 		console.log(activar);
+			if(resultado == 0 && activar==1)
 			{
 				var url = "<?php echo CController::createUrl('comprobanteContable/create'); ?>";
 	      		$.ajax(
@@ -291,8 +230,6 @@
 				               	$("#mensaje").empty();
 	           	   				$("#mensaje").removeClass("alert alert-dismissible alert-warning");
 	           	   				$("#mensaje").html(data);
-	           	   				$("#hiddenD").val("0");
-	           	   				$("#hiddenH").val("0");
 	      						/*Se eliminan las lineas contables*/
 					      		while(i>0)
 					      		{
@@ -304,7 +241,6 @@
 			}
 			else
 			{
-				$(".panel-primary").animate({scrollTop:0},'slow');
 				$("#mensaje").html("Problemas con las lineas");
 				$("#mensaje").addClass("alert alert-danger");
 			}
